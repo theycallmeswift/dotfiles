ad29cc2ae44d107b317c76aa002ec61afd403701
Swift <theycallmeswift@gmail.com>
2017-09-21 15:16:48 -0400

    Add QT and Postgres.app to path

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -10,6 +10,17 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
+# QT 5.5
+if [ -n "$(brew ls --versions qt@5.5)" ]; then
+  PATH="$(brew --prefix qt@5.5)/bin:$PATH"
+fi
+
+# Load Postgres.app
+export PG_APP_PATH="/Applications/Postgres.app/Contents/Versions/latest/bin"
+if [ -s "$PG_APP_PATH" ]; then
+  PATH="$PG_APP_PATH:$PATH"
+fi
+
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
